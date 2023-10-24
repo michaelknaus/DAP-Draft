b316cb0cc3c86657159a3d2a00b419a84fb39f85
Michael <121041087+michaelknaus@users.noreply.github.com>
2023-10-24 16:54:25 +0200

    finished changes for d3 graph

--- Assets/script.js
+++ Assets/script.js
@@ -9,9 +9,9 @@ d3.csv('Assets/data_wo_nan.csv').then(data => {
     });
 
     // Set the dimensions and margins of the graph
-    const margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    const margin = {top: 40, right: 30, bottom: 40, left: 50},
+        width = 480 - margin.left - margin.right,
+        height = 480 - margin.top - margin.bottom;
 
     // Append the svg object to the body of the page
     const svg = d3.select("#chart")
@@ -22,6 +22,14 @@ d3.csv('Assets/data_wo_nan.csv').then(data => {
             .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", 25)  // Position above the top margin
+        .attr("text-anchor", "middle")
+        .style("font-size", "20px")
+        .style("font-weight", "bold")
+        .text("Monthly temperatures Flughafen Graz");
+
     // Add X axis
     const x = d3.scaleTime()
         .domain(d3.extent(data, d => d.time))
@@ -30,12 +38,29 @@ d3.csv('Assets/data_wo_nan.csv').then(data => {
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom)
+        .style("text-anchor", "middle")
+        .text("Time");
+
     // Add Y axis
     const y = d3.scaleLinear()
         .domain([d3.min(data, d=>d.tmin), d3.max(data, d => d.tmax)])
-        .range([ height, 0 ]);
+        .range([ height, margin.top ]);
     svg.append("g")
         .call(d3.axisLeft(y));
+
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left)
+        .attr("x", -(height / 2))
+        .attr("dy", "1em")  // Adjust position slightly
+        .style("text-anchor", "middle")
+        .text("Temperature in Celsius");
+    
 
     // Add the line
     const line = svg.append("path")
@@ -57,33 +82,35 @@ d3.csv('Assets/data_wo_nan.csv').then(data => {
     }
 
     // Create a dropdown to toggle between different temperature types
-    const tempDropdown = d3.select("body")
+    const tempDropdown = d3.select(".dropdowns")
         .append("select")
         .attr("id", "tempDropdown")
         .on("change", function() {
             update(this.value, +document.getElementById('monthDropdown').value);
         });
-    tempDropdown.selectAll("option")
-        .data(["tmax", "tmin", "t"])
-        .enter()
-        .append("option")
-        .attr("value", d => d)
-        .text(d => d);
 
-    // Create a dropdown to toggle between different months
-    const monthDropdown = d3.select("body")
-        .append("select")
-        .attr("id", "monthDropdown")
-        .on("change", function() {
-            update(document.getElementById('tempDropdown').value, +this.value);
-        });
-    monthDropdown.selectAll("option")
-        .data(d3.range(0, 12))
-        .enter()
-        .append("option")
-        .attr("value", d => d)
-        .text(d => d3.timeFormat("%B")(new Date(2000, d)));  // Format month number as month name
+    
+        tempDropdown.selectAll("option")
+            .data(["tmax", "tmin", "t"])
+            .enter()
+            .append("option")
+            .attr("value", d => d)
+            .text(d => d);
 
-    // Initial rendering
-    update('tmax', 0);
+        // Create a dropdown to toggle between different months
+        const monthDropdown = d3.select(".dropdowns")
+            .append("select")
+            .attr("id", "monthDropdown")
+            .on("change", function() {
+                update(document.getElementById('tempDropdown').value, +this.value);
+            });
+        monthDropdown.selectAll("option")
+            .data(d3.range(0, 12))
+            .enter()
+            .append("option")
+            .attr("value", d => d)
+            .text(d => d3.timeFormat("%B")(new Date(2000, d)));  // Format month number as month name
+
+        // Initial rendering
+        update('tmax', 0);     
 });
